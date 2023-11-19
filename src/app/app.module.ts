3ddfccd63c657fed7cb138807e25c4aada130ab7
tsxepo <tsxepo@gmail.com>
2023-11-19 21:21:47 +0200

    Updated site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardsComponent } from './Components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
   ],
   imports: [
     BrowserModule,
